ba939bdee003b518dfc43473ade9b17207a76fcb
Wujue <wujue0115@gmail.com>
2024-03-01 19:18:12 +0800

    feat: add the Buttom component

---
+++ .vitepress/theme/components/atoms/Buttom.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    isPadding?: boolean;
+  }>(),
+  {
+    isPadding: true,
+  }
+);
+
+const { isPadding } = toRefs(props);
+const classOptions = computed(() => {
+  return [isPadding.value ? "p-0.5-3" : ""];
+});
+</script>
+
+<template>
+  <button :class="classOptions" btn-base flex items-center>
+    <slot name="before"></slot>
+    <slot></slot>
+    <slot name="after"></slot>
+  </button>
+</template>
